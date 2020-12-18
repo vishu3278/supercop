aaf1ee18573dfd21d2355e5b59224c909fea73ca
vishu3278 <vishu3278@gmail.com>
2020-12-18 23:39:22 +0530

    Update camera.js

--- camera.js
+++ camera.js
@@ -120,7 +120,7 @@ function openBrowser(url) {
 
 function openPrint(url) {
     console.log(url);
-    cordova.InAppBrowser.open(url, '_blank', "hidden=no,location=no,clearsessioncache=yes,clearcache=yes");
+    cordova.InAppBrowser.open(url, '_system', 'location=yes");
     //mywindow.document.write(content);
 }
 
